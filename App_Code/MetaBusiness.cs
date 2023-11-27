a9fb1af3c2a79a31f55590bd2a2ef1c8366c4183
kuramayouko <32944387+kuramayouko@users.noreply.github.com>
2023-11-27 10:36:40 -0300

    Pages code clean, database rework, meta api rework

---
+++ App_Code/MetaBusiness.cs
@@ -0,0 +1,26 @@
+namespace OpenSocials.App_Code
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+	public class MetaBusiness
+	{
+		public Facebook 
+		private readonly Token;
+		
+		private class Facebook
+		{
+
+		}
+
+		private class Instagram
+		{
+
+		}
+		
+	}
+}
